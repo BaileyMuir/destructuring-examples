39c01b692e4ba9322a7610dd5d703f9a45342fbb
BaileyMuir <BaileyIRMuir@outlook.com>
2021-09-19 19:31:03 +0000

    add destructuring examples.

--- destructuring.js
+++ destructuring.js
@@ -4,9 +4,50 @@
  */
 
 // Destructuring arrays
+let ages = [30, 26, 27];
+
+// let john = age[0];
+// let amy = age[1];
+// let joe = age[2];
+// console.log(john, amy, joe);
+// commented out is without
+
+let = [john, ann, joe] = ages
+console.log(john, ann, joe)
 
 // Destructuring objects
+let jobs = {
+    mike: "desighner",
+    jill: "developer",
+    alicia: "accountant",
+};
+let { mike, jill, alicia } = jobs;
+console.log(mike, jill, alicia);
 
 // Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+
+let [, , annNative, annSecondary] = languages
+
+console.log(annNative, annSecondary)
+
+let languages2 = {
+    firstlanguage: "english",
+    secondlanguage: "french",
+    thirdlanguage: "german",
+    fourthlanguage: "japanese",
+};
+
+let {firstlanguage, thirdlanguage} = languages2;
+console.log(firstlanguage, thirdlanguage);
 
 // Using rest parameter syntax
+
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favorite, secondFavorite, ...others] = fruits;
+
+console.log(favorite);
+console.log(secondFavorite);
+console.log(others);
